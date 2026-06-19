acf8d54d2b51a7c36860bbf4ca00ec787039fcd7
unknown <siddhantshukla455@gmail.com>
2026-06-19 16:15:34 +0530

    solved longest seq using brute force

--- 01_Arrays_and_Strings/longest_consecutive_sequence.py
+++ 01_Arrays_and_Strings/longest_consecutive_sequence.py
@@ -1,21 +1,15 @@
-nums=[1,2,90,98,3,27,91,93,92,95]
+nums=[1,2,90,98,3,27,91,93,92,95,94]
 
 def sols(nums):
-    max_lseq=1
     lseq=1
     
     for i in range(len(nums)):
-        if nums[i]==nums[i-1]:
-            continue
-        
-        if (st+1) in nums:
-            lseq=lseq+1
+        st=nums[i]
+        count=1
+        while (st+1 in nums):
+            count=count+1
             st=st+1
-        elif(max_lseq<lseq):
-            max_lseq=lseq
-        else:
-            st=nums[i]
-            lseq=1
+        lseq = max(lseq,count)
             
     print(lseq)
 sols(nums)
